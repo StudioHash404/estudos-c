dc8ffb8159aa7f38411e38493e4ae2d2f34c565a
NathanGitHub553 <ferreiranatham3@gmail.com>
2025-03-08 16:30:19 -0300

    update variaveis.c

---
+++ variaveis.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int taxa = 15;
+    
+    printf("\nGrupo 1:");
+    printf("\nJAO %d porcentos", taxa);
+    printf("\nABEL %d porcentos", taxa);
+    printf("\nMARIA %d porcentos", taxa);
+    
+    return 0;
+}
